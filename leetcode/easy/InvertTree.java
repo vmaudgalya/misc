3b656747c79a0d2364ec476c5ea63f85ec944585
Varun Maudgalya <mvarun321@gmail.com>
2015-09-26 14:44:02 -0700

    iterative invertTree & maxDepth

--- leetcode/easy/InvertTree.java
+++ leetcode/easy/InvertTree.java
@@ -9,25 +9,43 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+
 public class InvertTree {
-    public TreeNode invertTree(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-        Queue<TreeNode> queue = new LinkedList();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            TreeNode current = queue.poll();
-            TreeNode left = current.left;
-            current.left = current.right;
-            current.right = left;
-            if (current.left != null) {
-                queue.offer(current.left);
-            }
-            if (current.right != null) {
-                queue.offer(current.right);
-            }
-        }
-        return root;
+
+  /*************************
+  * Recursive implementation
+  * Time: O(n)
+  * Space: O(n)
+  ***************************/
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+      return null;
     }
+    TreeNode left = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(left);
+    return root;
+  }
+
+  public TreeNode iterativeInvertTree(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
+    Queue<TreeNode> queue = new LinkedList();
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      TreeNode current = queue.poll();
+      TreeNode left = current.left;
+      current.left = current.right;
+      current.right = left;
+      if (current.left != null) {
+        queue.offer(current.left);
+      }
+      if (current.right != null) {
+        queue.offer(current.right);
+      }
+    }
+    return root;
+  }
 }
--- leetcode/easy/MaxDepth.java
+++ leetcode/easy/MaxDepth.java
@@ -1,6 +1,6 @@
 // Given a binary tree, find its maximum depth.
 
-// The maximum depth is the number of nodes 
+// The maximum depth is the number of nodes
 // along the longest path from the root node down to the farthest leaf node.
 
 /**
@@ -13,18 +13,47 @@
  * }
  */
 
+public class MaxDepth {
 
-/************
+/********************************
+* Recursive implementation (DFS)
 * Time: O(n)
 * Space: O(n)
-*************/
-public class MaxDepth {
+*********************************/
   public int maxDepth(TreeNode root) {
     if (root == null) {
-        return 0;
+      return 0;
     }
     int left = 1 + maxDepth(root.left);
     int right = 1 + maxDepth(root.right);
     return (left < right ? right : left);
+  }
+
+/*******************************
+* Iterative implementation (BFS)
+* Time: O(n)
+* Space: O(n)
+********************************/
+  public int iterativeMaxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    Queue<TreeNode> q = new LinkedList<TreeNode>();
+    q.offer(root);
+    int depth = 0;
+    while (!q.isEmpty()) {
+      depth++;
+      int N = q.size();
+      for (int i = 0; i < N; i++) {
+        TreeNode current = q.poll();
+        if (current.left != null) {
+          q.offer(current.left);
+        }
+        if (current.right != null) {
+          q.offer(current.right);
+        }
+      }
+    }
+    return depth;
   }
 }
