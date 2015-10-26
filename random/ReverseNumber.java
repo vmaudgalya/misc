a17a6f06f4dd6a75075a71a3e1e02c340aa4fae8
Varun Maudgalya <mvarun321@gmail.com>
2015-10-25 22:46:26 -0700

    Reverse Number in-place

---
+++ random/ReverseNumber.java
@@ -0,0 +1,26 @@
+public class ReverseNumber {
+  
+  public static int reverse(int n) { //2015 -> 5102
+    if (n < 10) {
+      return n;
+    }
+    int reversed = 0;
+    while (n > 0) {
+      int digit = (n >= 10 ? n % 10 : n);
+      reversed = (reversed * 10) + digit;
+      n /= 10;
+    }
+    return reversed;
+  }
+
+  public static void main(String[] args) {
+    reverseTest();
+  }
+
+  public static void reverseTest() {
+    assert reverse(2015) == 5102;
+    assert reverse(0) == 0;
+    assert reverse(1234567) == 7654321;
+  }
+
+}
